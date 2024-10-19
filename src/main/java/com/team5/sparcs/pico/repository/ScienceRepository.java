e0a8a6c68fafb745819d8b906f58bc37e4e200d2
mugeon <pos04167@kakao.com>
2024-10-19 11:41:46 +0900

    feat : repository init

---
+++ src/main/java/com/team5/sparcs/pico/repository/ScienceRepository.java
@@ -0,0 +1,7 @@
+package com.team5.sparcs.pico.repository;
+
+import com.team5.sparcs.pico.domain.Scientist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScienceRepository  extends JpaRepository<Scientist, Long> {
+}
