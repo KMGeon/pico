4f3ab17fd466e0fe0371e427a36c1d5c3d0e97fa
mugeon <pos04167@kakao.com>
2024-10-19 17:54:36 +0900

    commit

--- src/main/java/com/team5/sparcs/pico/application/ScienceService.java
+++ src/main/java/com/team5/sparcs/pico/application/ScienceService.java
@@ -1,47 +1,66 @@
 package com.team5.sparcs.pico.application;
 
-import com.team5.sparcs.pico.domain.Scientist;
-import com.team5.sparcs.pico.dto.science.response.MainScienceResponse;
-import com.team5.sparcs.pico.dto.science.response.ScienceDetailResponse;
+import com.team5.sparcs.pico.domain.ScienceVO;
+import com.team5.sparcs.pico.dto.chatbot.request.ChatbotLogRequest;
+import com.team5.sparcs.pico.dto.chatbot.response.ChatbotView;
 import com.team5.sparcs.pico.repository.ScienceRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class ScienceService {
 
     private final ScienceRepository scienceRepository;
+    private final OpenAiChatModel openAiChatModel;
 
 
-    @Transactional
-    public Scientist createScience(HashMap<String, String> map) {
-        String name = map.get("name");
-        String url = map.get("url");
 
-        return scienceRepository.save(Scientist.builder()
-                .name(name)
-                .imgUrl(url)
-                .build());
 
+    public List<ScienceVO> selectScienceFindAll() {
+        return scienceRepository.selectScienceFindAll();
     }
 
-    @Transactional(readOnly = true)
-    public List<MainScienceResponse> selectScientistCard() {
-        return scienceRepository.findAll().stream()
-                .map(scientist -> {
-                    return MainScienceResponse.of(scientist);
-                }).toList();
+    public ScienceVO selectScienceDetail(String name) {
+        return scienceRepository.selectScienceDetail(name);
+    }
+
+    public ChatbotView selectChatBotView(String name) {
+
+        List<ScienceVO> scienceVOS = scienceRepository.selectChatbotView(name);
+
+        return ChatbotView.fromScienceVOList(scienceVOS);
+    }
+
+    public String insertChatBotLog(ChatbotLogRequest chatbotLogRequest) {
+        String roomId = chatbotLogRequest.roomId();
+        String request = chatbotLogRequest.request();
+        Integer i = scienceRepository.insertRequestChatBotLog(roomId, request);
+
+        String chatbotResponse = chatbot(request);
+        scienceRepository.insertResponseChatBotLog(roomId, chatbotResponse);
+        return chatbotResponse;
     }
 
 
-    @Transactional(readOnly = true)
-    public ScienceDetailResponse selectScientistDetail(String scienceId) {
-        return ScienceDetailResponse.of(scienceRepository.findById(Long.valueOf(scienceId))
-                .orElseThrow(() -> new RuntimeException("exception")));
+    public String chatbot(String request) {
+        PromptTemplate promptTemplate = new PromptTemplate(
+                "당신은 의사야 그래서 환자의 증상을 바탕으로 환저의 병을 예측해줘" +
+                        "환자가 자신의 증상을 말하면 너의 지식으로 잘 최대한 친절하게 알려줘." +
+                        "환자 : 배가 너무 아파요\n" +
+                        "답변 : 배탈인 것 같습니다.\n" +
+                        "이런 형식으로 대답해줘\n" +
+                        "환자: {message}\n"
+        );
+
+        String prompt = String.valueOf(promptTemplate.create(Map.of("message", request)));
+
+        return openAiChatModel.call(prompt);
     }
 }
--- src/main/java/com/team5/sparcs/pico/repository/ScienceRepository.java
+++ src/main/java/com/team5/sparcs/pico/repository/ScienceRepository.java
@@ -1,8 +1,51 @@
 package com.team5.sparcs.pico.repository;
 
-import com.team5.sparcs.pico.domain.Scientist;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.team5.sparcs.pico.domain.ScienceVO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
-public interface ScienceRepository  extends JpaRepository<Scientist, Long> {
+import java.util.HashMap;
+import java.util.List;
 
+@Repository
+
+public class ScienceRepository {
+
+    private final SqlSessionTemplate sqlSessionTemplate;
+
+    public ScienceRepository(@Qualifier("sqlSessionTemplate")SqlSessionTemplate sqlSessionTemplate) {
+        this.sqlSessionTemplate = sqlSessionTemplate;
+    }
+
+    public List<ScienceVO> selectScienceFindAll() {
+        return sqlSessionTemplate.selectList("science.selectScienceFindAll");
+    }
+
+    public ScienceVO selectScienceDetail(String name) {
+        HashMap<String, Object> param = new HashMap<>();
+        param.put("name",name);
+        return sqlSessionTemplate.selectOne("science.selectScienceDetail", param);
+    }
+
+    public List<ScienceVO> selectChatbotView(String name){
+        HashMap<String, Object> param = new HashMap<>();
+        param.put("name",name);
+        return sqlSessionTemplate.selectOne("science.selectChatbotView", param);
+    }
+
+    public Integer insertRequestChatBotLog(String roomId, String request) {
+        HashMap<String, Object> param = new HashMap<>();
+        param.put("roomId",roomId);
+        param.put("request",request);
+        return sqlSessionTemplate.insert("science.insertRequestChatBotLog", param);
+    }
+
+
+    public Integer insertResponseChatBotLog(String roomId, String chatbotResponse) {
+        HashMap<String, Object> param = new HashMap<>();
+        param.put("roomId",roomId);
+        param.put("response", chatbotResponse);
+        return sqlSessionTemplate.insert("science.insertResponseChatBotLog", param);
+    }
 }
