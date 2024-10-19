d9a951e17490aea68da51b9c66596ce48d833e91
mugeon <pos04167@kakao.com>
2024-10-20 08:03:53 +0900

    commit

--- src/main/java/com/team5/sparcs/pico/controller/ChatController.java
+++ src/main/java/com/team5/sparcs/pico/controller/ChatController.java
@@ -3,6 +3,7 @@ package com.team5.sparcs.pico.controller;
 import com.team5.sparcs.pico.application.ChatService;
 import com.team5.sparcs.pico.dto.chatbot.request.ChatbotLogRequest;
 import com.team5.sparcs.pico.dto.chatbot.response.SummaryResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +16,13 @@ import java.util.List;
 public class ChatController {
     private final ChatService chatService;
 
+    @Operation(summary = "챗봇", description = "모든 대화의 AI와 자유롭게 이야기 할 수 있다.")
     @PostMapping("/chat/principle")
     public String chatbotTalk(@RequestBody ChatbotLogRequest chatbotLogRequest){
         return chatService.chatbotTalk(chatbotLogRequest);
     }
 
+    @Operation(summary = "챗봇", description = "모든 내용을 정리한다.")
     @PostMapping("/chat/summery")
     public SummaryResponse summery(@RequestBody ChatbotLogRequest chatbotLogRequest){
         return chatService.summery(chatbotLogRequest);
