2945b19306f1a94b281d2536dea2a7ebc8ca5ddc
mugeon <pos04167@kakao.com>
2024-10-19 23:13:36 +0900

    commit

--- src/main/java/com/team5/sparcs/pico/controller/ScienceController.java
+++ src/main/java/com/team5/sparcs/pico/controller/ScienceController.java
@@ -7,6 +7,7 @@ import com.team5.sparcs.pico.dto.chatbot.response.ChatbotView;
 import com.team5.sparcs.pico.dto.science.MainDetailResponse;
 import com.team5.sparcs.pico.dto.science.MainResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +34,7 @@ public class ScienceController {
     @Operation(summary = "main 1-2", description = "1번에서 과학자 선택하면 과학자 이름을 넘겨서 디테일 조회")
     @Logging(action = "get")
     @GetMapping("/science/detail")
-    public ResponseEntity<MainDetailResponse> selectScienceDetail(@RequestParam(value = "name") String name) {
+    public ResponseEntity<MainDetailResponse> selectScienceDetail(@Parameter(name = "name", description = "과학자 이름") @RequestParam(value = "name") String name) {
         return ResponseEntity.ok(scienceService.selectScienceDetail(name));
     }
 
@@ -41,20 +42,18 @@ public class ScienceController {
     @Operation(summary = "main 2", description = "이름을 받아서 챗봇 처음에 데이터 전달 VIEW")
     @Logging(action = "get")
     @GetMapping("/chatbot")
-    public ResponseEntity<ChatbotView> selectChatBotView(@RequestParam(value = "name") String name) {
+    public ResponseEntity<ChatbotView> selectChatBotView(@Parameter(name = "name", description = "과학자 이름") @RequestParam(value = "name") String name) {
         ChatbotView chatbotView = scienceService.selectChatBotView(name);
         return ResponseEntity.ok(chatbotView);
     }
 
 
-    @Operation(summary = "main 2 - 자유", description = "이름을 받아서 챗봇 처음에 데이터 전달 VIEW")
+    @Operation(summary = "main 2 - 자유", description = "Step [1,2,3] -> 1 : 원리 1 / 2 = 원리 2 / 3 = 자율 chat")
     @Logging(action = "post")
     @PostMapping("/chatbot")
-    public ResponseEntity<String> insertChatBotLog(@RequestBody ChatbotLogRequest chatbotLogRequest){
+    public ResponseEntity<String> insertChatBotLog(@Parameter(name = "아이디, 과학자, 요청, step", description = "request는 회원의 input , step은 1,2,3으로 구분해서 각 단계에 요청에 포함되게 처리") @RequestBody ChatbotLogRequest chatbotLogRequest) {
         String rtn = scienceService.insertChatBotLog(chatbotLogRequest);
         return ResponseEntity.ok(rtn);
     }
 //    ======================= 도감 ============================
-
-
 }
