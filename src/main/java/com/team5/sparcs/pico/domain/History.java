511345267d4cb090270960c7632f151199618b51
mugeon <pos04167@kakao.com>
2024-10-19 11:42:09 +0900

    feat : dummy api create

---
+++ src/main/java/com/team5/sparcs/pico/domain/History.java
@@ -0,0 +1,8 @@
+package com.team5.sparcs.pico.domain;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class History extends BaseEntity{
+    private String summation;
+}
---
+++ src/main/java/com/team5/sparcs/pico/domain/ScientificPrinciple.java
@@ -0,0 +1,9 @@
+package com.team5.sparcs.pico.domain;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class ScientificPrinciple extends BaseEntity{
+    private String name;
+    private String description;
+}
